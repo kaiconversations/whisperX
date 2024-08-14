06de396daa80ab3753ad6454c8c015248131e617
Ben Naylor <65810656+ben256@users.noreply.github.com>
2024-08-14 14:58:08 +0100

    added faster whisper pipeline (#4)
    Add additional inference method using faster-whisper batched inference pipeline

--- whisperx/__init__.py
+++ whisperx/__init__.py
@@ -1,4 +1,5 @@
 from .transcribe import load_model
 from .alignment import load_align_model, align, load_tokenizer_model
 from .audio import load_audio
+from .asr import WhisperXPipeline, BatchedFasterWhisperPipeline
 from .diarize import assign_word_speakers, DiarizationPipeline, SpeechSeparationPipeline
